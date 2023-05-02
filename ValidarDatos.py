06e26b4df36190be2b38fc49f2254693c9c277b8
Francisco Tamasi <frantamasi1@gmail.com>
2023-05-02 01:10:50 -0300

    correccion float
    arreglo para cuando se ingresa mas de un punto

--- ValidarDatos.py
+++ ValidarDatos.py
@@ -1,18 +1,23 @@
 def Es_numero(dato_ingresado): #hacer que acepte numeros float
     resultado = False
     parteentera=dato_ingresado.find(".") #asigna la posicion donde se encuentra el punto en el string
-   
+    
+
     if(dato_ingresado.isdigit() == True):
         resultado = True
     
     elif(parteentera != -1): #si entra al estring, quiere decir que es un float
+        
         cantidad_entera = (dato_ingresado[:parteentera]) #asigna una subcadena desde el inicio del string hasta donde esta el punto
-        cantidad_decimal=(dato_ingresado[parteentera:]) #asigna una subcadena desde el punto del string hasta el final
-        numero_en_string = f"{cantidad_entera}"+f"{cantidad_decimal}" #los concatena 
-        numero_float = float(numero_en_string) #lo pasa a float
-
-        resultado = True
-    
+        cantidad_decimal=(dato_ingresado[parteentera+1:]) #asigna una subcadena desde el punto del string hasta el final
+        #numero_en_string = f"{cantidad_entera}"+"."+f"{cantidad_decimal}" #los concatena 
+        
+        otroPunto = cantidad_decimal.find(".") #vuelve a chequear que no haya otro punto
+        if(otroPunto != -1): #si hay otro retorna falso
+            resultado = False
+        else:
+            resultado = True
+            
     elif(dato_ingresado.find("-") != -1): #identifica si es un numero negativo
         resultado = True
     
@@ -46,3 +51,6 @@ def numero_entero(dato_ingresado): #hacer que acepte numeros float
         resultado = True
 
     return resultado
+
+def numero_flotante(dato):
+    pass
