510d9bd82f2b60d0c10c0d1804b15efdaf881430
Francisco Tamasi <frantamasi1@gmail.com>
2023-05-09 19:31:25 -0300

    primer script de binario
    conversion de decimal a binario

--- Menu.py
+++ Menu.py
@@ -2,45 +2,6 @@ from os import system
 import Decimal  
 import ValidarDatos as VD
 
-def MenuPrincipal():
-    condicion = False
-    numeros = []
-    operacion = 0
-    while(condicion == False):
-        print("             CALCULADORA")
-        print("")
-        print("¿Con que tipo de base desea trabajar?")
-        print("")
-        print("1.   Decimal")
-        print("2.   Binario")
-        print("3.   Octal")
-        print("4.   Hexadecimal")
-        print("")
-        base = (input("Ingrese el numero de base que desea utilizar: "))
-        
-        if(VD.numero_entero(base) == True): #hacer que lo invoque calculadora
-            base=int(base)
-            if(base == 1):
-                numeros=Decimal.Operandos_En_Decimal() #retorna los numeros que se utilizan
-                operacion = Menu_Tipo_De_Operacion_Decimal() #retorna la operacion a realizar           
-                Decimal.operacion(numeros, operacion)
-                system("cls")
-                sigue = realizar_otra() #pregunta si va a seguir ejecutando
-                if(sigue == False):
-                    condicion = True
-                
-            elif(base == 2):
-                pass
-            elif(base == 3):
-                pass
-            elif(base == 4):
-                pass
-            else:
-                input("El numero ingresado no es valido ")
-                system("cls")
-        else:
-            input("Eso no es un numero, ingrese un numero para continuar ")
-            system("cls")
 
 def Menu_Tipo_De_Operacion_Decimal():
     system("cls")
